6000ed715837cc80bce9dbe4bc59056ec6bd5f8b
elbelel <umarelbelel@gmail.com>
2020-06-16 00:34:05 +0100

    fixes routes

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './src/Home'
@@ -9,18 +8,6 @@ import SignIn from './src/SignIn';
 import SignUp from './src/SignUp';
 
 
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
 const Stack = createStackNavigator();
 
 function App() {
@@ -28,7 +15,6 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="Screen2" component={Screen2}/>
         <Stack.Screen name="Initializing" component={Initialising}/>
         <Stack.Screen name="SignIn" component={SignIn}/>
