68aca9ff4b76d902b71597afaeffe25acc0c983b
Chriswenzy <chrischukwuebuka85@gmail.com>
2020-06-18 18:12:39 +0100

    NEW: finish login page

--- src/SignIn.js
+++ src/SignIn.js
@@ -4,8 +4,9 @@ import {
   Text,
   StyleSheet,
   TextInput,
-  Button,
-  AsyncStorage
+  AsyncStorage,
+  TouchableOpacity,
+  Image
 } from 'react-native'
 
 import { goHome } from './navigation'
@@ -31,28 +32,57 @@ export default class SignIn extends React.Component {
   }
   render() {
     return (
-      <View style={styles.container}>
+
+      <TouchableOpacity  onPress={this.props.onPress} style={styles.container} >
+
+         <View style={styles.img}> 
+            <Image source={require('../assets/logo.png')} style={styles.logo}  />
+        </View>
+
+        <Text style={styles.titleText}>Welcome back</Text>
+
+        <View >
+        <Text style={styles.label}>Email</Text>
         <TextInput
           style={styles.input}
-          placeholder='Email'
-          autoCapitalize="none"
+          placeholder='Please enter email here'
+          autoCapitalize="characters"
           autoCorrect={false}
-          placeholderTextColor='white'
+          placeholderTextColor='#B3B4B9'
           onChangeText={val => this.onChangeText('email', val)}
         />
+
+        <Text style={styles.label}>Password</Text>
         <TextInput
           style={styles.input}
-          placeholder='Password'
-          autoCapitalize="none"
+          placeholder='Please enter password here'
+          autoCapitalize="characters"
           secureTextEntry={true}
-          placeholderTextColor='white'
+          placeholderTextColor='#B3B4B9'
           onChangeText={val => this.onChangeText('password', val)}
         />
-        <Button
-          title='Sign In'
-          onPress={this.signIn}
-        />
+
+        <Text style={styles.label1}
+        onPress={() => {
+          this.props.navigation.navigate('SignUp');
+        }}
+        
+        >Don’t have an account? Sign up</Text>
+
+          <View style={styles.container}>
+              <View style={styles.btnContainer}>
+                    <Text
+                    style={styles.btnText }
+                    onPress={() => {
+                      this.props.navigation.navigate('SignIn');
+                    }}>
+                    Login
+                  </Text>
+              </View>
+          </View>
       </View>
+      </TouchableOpacity>
+      
     )
   }
 }
@@ -60,18 +90,86 @@ export default class SignIn extends React.Component {
 const styles = StyleSheet.create({
   input: {
     width: 350,
-    fontSize: 18,
+    fontSize: 14,
     fontWeight: '500',
     height: 55,
-    backgroundColor: 'red',
+    backgroundColor: '#F7F7F7',
     margin: 10,
-    color: 'white',
+    color: 'black',
     padding: 8,
     borderRadius: 5,
+
   },
+
+  img:{
+    marginTop: -130,
+    },
+
+  logo:{
+    flex: 1,
+    width: 200,
+    height: 240,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: -60,
+    },
+
+    titleText:{
+      fontSize: 20,
+      fontWeight: "bold",
+      lineHeight: 10,
+      textAlign: "center",
+      color: '#364471',
+      marginBottom: 30,
+    },
+
+  label:{
+   marginLeft: 10, 
+   color: '#364471',
+   fontSize: 18,
+   fontWeight: "bold",
+   marginTop: 10,
+  },
+
+  label1:{
+    marginLeft: 10, 
+    color: '#364471',
+    fontSize: 13,
+    fontWeight: "bold",
+    marginTop: 10,
+   },
+
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+
+  btnContainer: {
+    flex:1,
+    backgroundColor: '#364471',
+    paddingVertical:10,
+    paddingHorizontal:10,
+    borderRadius: 25,
+    textAlign: 'center',
+    width: 200,
+
+    shadowColor: "#000",
+    shadowOffset: {
+	  width: 0,
+	  height: 4,
+                },
+    shadowOpacity: 0.30,
+    shadowRadius: 4.65,
+    elevation: 8,
+    marginTop: 100,  
+  },
+
+  btnText:{
+    color: 'white',
+    fontSize: 18,
+    
+    textAlign: "center",
+  },
 })
